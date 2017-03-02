fef0a0faa37d352b3381888f6b42e34c263841e1
Fernando <pachecofgf@gmail.com>
2017-03-02 17:49:30 -0500

    Script geometria 4

---
+++ Migracion_Actualizacion_IMSMA.py
@@ -0,0 +1,36 @@
+import arcpy, os, sys
+
+GeodatabaseIMSMA=r"D:\APN\IMSMA.gdb"
+GeodatabaseSDEImsma=r""
+
+fcprueba= GeodatabaseIMSMA+ os.sep+ "Hazard_Reductions_polygon"
+def ListaCampos(Feat):
+    ListaFinal=[]
+    ListaInit= arcpy.ListFields(Feat)
+    for field in ListaInit:
+        if field.editable==True and field.type!="Geometry" and field.type!="OID":
+            ListaFinal.append(field.name)
+    ListaFinal.append('SHAPE@')
+    return ListaFinal
+
+def unique_values(table, field):
+    with arcpy.da.SearchCursor(table, [field]) as cursor:
+        return sorted({row[0] for row in cursor})
+
+print ListaCampos(fcprueba)
+
+def IngresarRegistrosNuevos(Feat,row,fields):
+    cursor = arcpy.da.InsertCursor(Feat, fields)
+    cursor.insertRow(row)
+
+def actualizarValores(Featin, FeatOut, fields):
+    with arcpy.da.SearchCursor(Featin, fields) as cursor:
+    # For each row, evaluate the WELL_YIELD value (index position
+    # of 0), and update WELL_CLASS (index position of 1)
+        for row in cursor:
+            for rowvalue in row:
+            # Update the cursor with the updated list
+                cursor.updateRow(row)
+
+arcpy.env.workspace=GeodatabaseIMSMA
+ListaFeatEntrada= arcpy.ListFeatureClasses()
