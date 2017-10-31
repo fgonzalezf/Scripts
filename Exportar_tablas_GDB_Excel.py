f0cb4b9f43d1fc1312efb1399505e9f19462d1c5
APN <APN@gmail.com>
2017-10-30 19:21:04 -0500

    excel

--- Exportar_tablas_GDB_Excel.py
+++ Exportar_tablas_GDB_Excel.py
@@ -1,6 +1,7 @@
 import arcpy, os, sys
 from xlutils.copy import copy
 from xlrd import open_workbook
+from win32com.client import Dispatch
 
 geodatabase=r"C:\Users\APN\Documents\SGC\Muestras\Muestras.gdb\Muestras"
 Carpetasalida=r"C:\Users\APN\Documents\SGC\Muestras\Excel"
@@ -17,12 +18,21 @@ arcpy.env.workspace=Carpetasalida
 
 listaExcel= arcpy.ListFiles()
 X=0
-xlsBase=None
+xlsBase=""
+xl = Dispatch("Excel.Application")
+xl.Visible = True
 for xls in listaExcel:
     if X==0:
-        xlsBase=open_workbook(xls)
+        xlsBase=Carpetasalida+os.sep+xls
     else:
-        xlsUnion=open_workbook(xls)
+        xlsUnion=Carpetasalida+os.sep+xls
+
+        wb1 = xl.Workbooks.Open(Filename=xlsUnion)
+        wb2 = xl.Workbooks.Open(Filename=xlsBase)
+        ws1 = wb1.Worksheets(1)
+        ws1.Copy(Before=wb2.Worksheets(1))
+        wb2.Close(SaveChanges=True)
     X=1
+xl.Quit()
 
 
