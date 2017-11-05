510a538ae76f90071e77325260bd05c70587a1d1
Fernando <pachecofgf@gmail.com>
2017-11-05 16:32:14 -0500

    IMAGENES TERMALES

--- Migracion_Muestras.py
+++ Migracion_Muestras.py
@@ -7,6 +7,17 @@ def getSheetName(file_name):
     pointSheets = TeamPointWorkbook.sheet_names()
 
     return pointSheets
+
+def getIndexField(fieldind,listFields):
+    indx=-1
+    indextemp=0
+    for field in listFields:
+        if field==fieldind:
+            indx=indextemp
+            break
+        indextemp=indextemp+1
+    return indx
+
 def Campos(Feat):
     desc = arcpy.Describe(Feat)
     tipo= desc.dataType
@@ -23,32 +34,42 @@ def Campos(Feat):
     return Lista
 #Variables
 
-xlsFile=r"C:\Users\APN\Documents\SGC\Muestras\Excel\Libro_Indice_Cargue (1).xls"
-GeodatabaseModelo=r"C:\Users\APN\Documents\SGC\Muestras\Muestras.gdb\Muestras"
+xlsFile=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Libro_indice.xls"
+GeodatabaseModelo=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Origen_Bogota\mg100k.gdb\Muestras"
 
 print(getSheetName(xlsFile))
 
 ListaHojas = getSheetName(xlsFile)
 
+
+
 for hoja in ListaHojas:
+    edit = arcpy.da.Editor(os.path.dirname(GeodatabaseModelo))
+    edit.startEditing ()
+    edit.startOperation()
     arcpy.ExcelToTable_conversion(xlsFile,GeodatabaseModelo+"_temp",hoja)
     tablaEntrada= GeodatabaseModelo+"_temp"
     FeatureClassSalida=GeodatabaseModelo+os.sep+hoja
     camposEntrada=Campos(tablaEntrada)
     CamposSalida=Campos(FeatureClassSalida)
     cursorIns = arcpy.da.InsertCursor(FeatureClassSalida, CamposSalida)
+    print camposEntrada
+    print CamposSalida
     with arcpy.da.SearchCursor(tablaEntrada, camposEntrada) as cursor:
-        print cursor.fields
+        #print cursor.fields
         for row in cursor:
-            pass
-            #rowin=row
-            #rowin = list(rowin)
-            #rowin.insert(0,pointCentroid)
-            #rowin = tuple(rowin)
-            #cursorIns.insertRow(row)
-
-
+            indxX=getIndexField("Este",cursor.fields)
+            indxY=getIndexField("Norte",cursor.fields)
+            rowin=row
+            rowin = list(rowin)
+            pointCentroid=(row[indxX],row[indxY])
+            rowin.insert(0,pointCentroid)
+            rowin = tuple(rowin)
+            cursorIns.insertRow(rowin)
 
     #print camposEntrada
     #print CamposSalida
+    edit.stopOperation()
+    edit.stopEditing("True")
     arcpy.Delete_management(tablaEntrada)
+
