1eb5a8488089028c2e459a700ef457249bde4130
Fernando <pachecofgf@gmail.com>
2017-11-06 20:33:16 -0500

    IMAGENES TERMALES

--- Migracion_Muestras.py
+++ Migracion_Muestras.py
@@ -7,6 +7,13 @@ def getSheetName(file_name):
     pointSheets = TeamPointWorkbook.sheet_names()
 
     return pointSheets
+def normalizarDatos(row):
+    row=list(row)
+    for i in range(len(row)):
+        if row[i]=="":
+            row[i]=None
+    row =tuple(row)
+    return row
 
 def getIndexField(fieldind,listFields):
     indx=-1
@@ -35,7 +42,7 @@ def Campos(Feat):
 #Variables
 
 xlsFile=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Libro_indice.xls"
-GeodatabaseModelo=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Origen_Bogota\mg100k.gdb\Muestras"
+GeodatabaseModelo=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Origen_Bogota\Muestras.mdb\Muestras"
 
 print(getSheetName(xlsFile))
 
@@ -44,14 +51,16 @@ ListaHojas = getSheetName(xlsFile)
 
 
 for hoja in ListaHojas:
-    edit = arcpy.da.Editor(os.path.dirname(GeodatabaseModelo))
-    edit.startEditing ()
-    edit.startOperation()
-    arcpy.ExcelToTable_conversion(xlsFile,GeodatabaseModelo+"_temp",hoja)
-    tablaEntrada= GeodatabaseModelo+"_temp"
+
+    arcpy.ExcelToTable_conversion(xlsFile,os.path.dirname(GeodatabaseModelo)+os.sep+"temp",hoja)
+    tablaEntrada= os.path.dirname(GeodatabaseModelo)+os.sep+"temp"
     FeatureClassSalida=GeodatabaseModelo+os.sep+hoja
     camposEntrada=Campos(tablaEntrada)
     CamposSalida=Campos(FeatureClassSalida)
+
+    edit = arcpy.da.Editor(os.path.dirname(GeodatabaseModelo))
+    edit.startEditing()
+    edit.startOperation()
     cursorIns = arcpy.da.InsertCursor(FeatureClassSalida, CamposSalida)
     print camposEntrada
     print CamposSalida
@@ -65,12 +74,11 @@ for hoja in ListaHojas:
             pointCentroid=(row[indxX],row[indxY])
             rowin.insert(0,pointCentroid)
             rowin = tuple(rowin)
+            rowin=normalizarDatos(rowin)
             cursorIns.insertRow(rowin)
-
-
     #print camposEntrada
     #print CamposSalida
     edit.stopOperation()
-    edit.stopEditing("True")
+    edit.stopEditing()
     arcpy.Delete_management(tablaEntrada)
 
