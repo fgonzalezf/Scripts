a6f7b7b7bb3b619cd48e56dc3f03259378ae8519
Equipo <pachecofgf@gmail.com>
2017-11-07 00:08:13 -0500

    sql bip metadato2

--- Migracion_Muestras.py
+++ Migracion_Muestras.py
@@ -1,19 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import arcpy, os,sys
 import xlrd as xl
 arcpy.env.overwriteOutput=True
+
+def to_unicode_or_bust(obj, encoding='utf-8'):
+    if isinstance(obj, basestring):
+        if not isinstance(obj, unicode):
+            obj = unicode(obj, encoding)
+    return obj
 def getSheetName(file_name):
     pointSheetObj = []
     TeamPointWorkbook = xl.open_workbook(file_name)
     pointSheets = TeamPointWorkbook.sheet_names()
 
     return pointSheets
-def normalizarDatos(row):
+def normalizarDatos(row,longitud):
     row=list(row)
     for i in range(len(row)):
         if row[i]=="":
             row[i]=None
+        elif(longitud[i]>0):
+            if isinstance(row[i], basestring):
+                row[i] = (row[i]).encode('utf-8')[0:longitud[i]]
+            else:
+                row[i] = str(row[i])[0:longitud[i]]
+
     row =tuple(row)
     return row
+
 
 def getIndexField(fieldind,listFields):
     indx=-1
@@ -25,24 +40,43 @@ def getIndexField(fieldind,listFields):
         indextemp=indextemp+1
     return indx
 
-def Campos(Feat):
+def lenCampos(Feat):
     desc = arcpy.Describe(Feat)
     tipo= desc.dataType
     Lista=[]
     ListaCampos=arcpy.ListFields(Feat)
     if tipo=="FeatureClass":
         if desc.shapeType=="Point":
+            Lista.append(0)
+        else:
+            Lista.append(0)
+    for fld in ListaCampos:
+        if fld.editable==True and fld.type!="Geometry":
+            if fld.type=="String":
+                Lista.append(fld.length)
+            else:
+                Lista.append(0)
+
+    return Lista
+#Variables
+def Campos(Feat):
+    desc = arcpy.Describe(Feat)
+    tipo = desc.dataType
+    Lista = []
+    ListaCampos = arcpy.ListFields(Feat)
+    if tipo == "FeatureClass":
+        if desc.shapeType == "Point":
             Lista.append('SHAPE@XY')
         else:
             Lista.append('SHAPE@')
     for fld in ListaCampos:
-        if fld.editable==True and fld.type!="Geometry":
+        if fld.editable == True and fld.type != "Geometry":
             Lista.append(fld.name)
     return Lista
-#Variables
 
-xlsFile=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Libro_indice.xls"
-GeodatabaseModelo=r"C:\Users\Fernando\Documents\Muestras\CARGUE\Origen_Bogota\Muestras.mdb\Muestras"
+
+xlsFile=r"C:\Users\Equipo\Documents\Muestras\Libro_Indice_Cargue.xls"
+GeodatabaseModelo=r"C:\Users\Equipo\Documents\Muestras\mg100k.gdb\Muestras"
 
 print(getSheetName(xlsFile))
 
@@ -57,13 +91,13 @@ for hoja in ListaHojas:
     FeatureClassSalida=GeodatabaseModelo+os.sep+hoja
     camposEntrada=Campos(tablaEntrada)
     CamposSalida=Campos(FeatureClassSalida)
+    longitudCamposSal=lenCampos(FeatureClassSalida)
 
     edit = arcpy.da.Editor(os.path.dirname(GeodatabaseModelo))
-    edit.startEditing()
-    edit.startOperation()
+    #edit.startEditing(True,False)
+    #edit.startOperation()
     cursorIns = arcpy.da.InsertCursor(FeatureClassSalida, CamposSalida)
-    print camposEntrada
-    print CamposSalida
+    print hoja
     with arcpy.da.SearchCursor(tablaEntrada, camposEntrada) as cursor:
         #print cursor.fields
         for row in cursor:
@@ -71,14 +105,15 @@ for hoja in ListaHojas:
             indxY=getIndexField("Norte",cursor.fields)
             rowin=row
             rowin = list(rowin)
-            pointCentroid=(row[indxX],row[indxY])
+            pointCentroid=(float(row[indxX]),float(row[indxY]))
             rowin.insert(0,pointCentroid)
             rowin = tuple(rowin)
-            rowin=normalizarDatos(rowin)
+            rowin=normalizarDatos(rowin,longitudCamposSal)
+            print rowin
             cursorIns.insertRow(rowin)
     #print camposEntrada
     #print CamposSalida
-    edit.stopOperation()
-    edit.stopEditing()
+    #edit.stopOperation()
+    #edit.stopEditing(True)
     arcpy.Delete_management(tablaEntrada)
 
