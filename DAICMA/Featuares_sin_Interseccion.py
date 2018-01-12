3196de5afab97af35bcac0b1aca6290b4ae41f38
Fernando <pachecofgf@gmail.com>
2018-01-12 01:02:23 -0500

    IMAGENES TERMALES

--- DAICMA/Featuares_sin_Interseccion.py
+++ DAICMA/Featuares_sin_Interseccion.py
@@ -1,9 +1,15 @@
 import arcpy, os ,sys
 
-capaEntrada =r""
-capaInterseccion=r""
+capaEntrada =r"D:\APN\mapasss\Scripts\GDB\Bk_GDB.gdb\DAICMA\Areas_Canceladas"
+capaInterseccion=r"D:\APN\mapasss\Scripts\GDB\Bk_GDB.gdb\DAICMA\Area_Peligrosa"
+excelSalida=r"D:\APN\mapasss\Scripts\GDB\Excel_Areas_Peligrosas_sin_Areas_Canceladas.xls"
 
 layerEntrada= arcpy.MakeFeatureLayer_management(capaEntrada,"layerEntrada")
 layerInterseccion= arcpy.MakeFeatureLayer_management(capaInterseccion,"layerInterseccion")
 
-arcpy.SelectLayerByLocation_management("layerEntrada",)
+arcpy.SelectLayerByLocation_management("layerEntrada","INTERSECT","layerInterseccion","","","INVERT")
+
+arcpy.TableToExcel_conversion("layerEntrada",excelSalida)
+
+arcpy.Delete_management("layerEntrada")
+arcpy.Delete_management("layerInterseccion")
