cc0398119fd839c8a3da0d5269763ae7ea2ca051
APN <APN@gmail.com>
2018-01-15 18:22:27 -0500

    validador oaipmh

--- DAICMA/Featuares_sin_Interseccion.py
+++ DAICMA/Featuares_sin_Interseccion.py
@@ -1,8 +1,8 @@
 import arcpy, os ,sys
 
-capaEntrada =r"D:\APN\mapasss\Scripts\GDB\Bk_GDB.gdb\DAICMA\Areas_Canceladas"
-capaInterseccion=r"D:\APN\mapasss\Scripts\GDB\Bk_GDB.gdb\DAICMA\Area_Peligrosa"
-excelSalida=r"D:\APN\mapasss\Scripts\GDB\Excel_Areas_Peligrosas_sin_Areas_Canceladas.xls"
+capaEntrada =r"C:\Users\APN\Documents\APN\GDB\Bk_GDB.gdb\DAICMA\Areas_Canceladas"
+capaInterseccion=r"C:\Users\APN\Documents\APN\GDB\Bk_GDB.gdb\DAICMA\Area_Peligrosa"
+excelSalida=r"C:\Users\APN\Documents\APN\GDB\Excel_Areas_Canceladas_Sin_Areas_Peligrosas.xls"
 
 layerEntrada= arcpy.MakeFeatureLayer_management(capaEntrada,"layerEntrada")
 layerInterseccion= arcpy.MakeFeatureLayer_management(capaInterseccion,"layerInterseccion")
