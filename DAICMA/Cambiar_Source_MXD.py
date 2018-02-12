7719cb6b8b3cfb9545873cc7cd669d4150bb46c4
APN <APN@gmail.com>
2018-02-12 16:52:06 -0500

    validador oaipmh

--- DAICMA/Cambiar_Source_MXD.py
+++ DAICMA/Cambiar_Source_MXD.py
@@ -1,9 +1,22 @@
 import arcpy, os,sys
 
-arcpy.env.workspace=r""
+CarpetaEntrada=r"C:\Users\APN\Documents\APN\Migracion_Server\Zonas"
+CarpetaSalida=r"C:\Users\APN\Documents\APN\Migracion_Server\Zonas2"
+arcpy.env.workspace=CarpetaEntrada
 
+pathin=r"C:\Users\maicolvelasquez\AppData\Roaming\Esri\Desktop10.2\ArcCatalog\SDE_VISORES.sde"
+pathOut=r"C:\Users\APN\Documents\APN\Migracion_Server\Scripts\GDB\Bk_GDB.gdb"
 
-mxd = arcpy.mapping.MapDocument(r"C:\Project\Project.mxd")
-mxd.findAndReplaceWorkspacePaths(r"C:\Project\Data", r"C:\Project\Data2")
-mxd.saveACopy(r"C:\Project\Project2.mxd")
-del mxd
+ListaCarpetas=arcpy.ListWorkspaces("*", "Folder")
+
+for folder in ListaCarpetas:
+    print folder
+    arcpy.CreateFolder_management(CarpetaSalida,os.path.basename(folder))
+    arcpy.env.workspace = folder
+    ListaMxd = arcpy.ListFiles("*.mxd")
+    for mxdfile in ListaMxd:
+        print mxdfile
+        mxd = arcpy.mapping.MapDocument(folder+ os.sep+ mxdfile)
+        mxd.findAndReplaceWorkspacePaths(pathin, pathOut)
+        mxd.saveACopy(CarpetaSalida+ os.sep +os.path.basename(folder)+ os.sep+ mxdfile,"10.1")
+        del mxd
