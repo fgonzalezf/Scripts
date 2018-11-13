4f13c2c1d0a088d011b7368c34b6d213d0c2b6d3
Fernando <pachecofgf@gmail.com>
2018-11-12 22:46:30 -0500

    IMAGENES TERMALES

--- IGAC2018/Grilla_Muestra.py
+++ IGAC2018/Grilla_Muestra.py
@@ -1,14 +1,22 @@
 import arcpy, os,sys,random
 
-CarpetaSalida=r"C:\temp"
-Escala=10000
+
+#Planchas=r"C:\Users\APN\Documents\IGAC\Planchas_10K_Planas\central.shp"
+#campo=r""
+#plancha="8IIIB1"
+#CarpetaSalida=r"C:\temp"
+#Escala=10000
+Planchas=sys.argv[1]
+campo=sys.argv[2]
+plancha=sys.argv[3]
+CarpetaSalida=sys.argv[4]
+Escala=int(sys.argv[5])
+
+
+
 NumeroMuestra=20
 Filas=10
 Columnas=15
-
-Planchas=r"C:\Users\APN\Documents\IGAC\Planchas_10K_Planas\central.shp"
-plancha="8IIIB1"
-salida=r"C:\temp"
 arcpy.env.overwriteOutput=True
 
 def CrearSalida(ruta,nombre):
@@ -23,9 +31,9 @@ def FindIdentificador(Feat):
         if field.type=="OID":
             Identificador=field.name
     return Identificador
-def QueryPlancha (Feat, plancha):
-    fields=['SHAPE@WKT',"PLANCHA","SHAPE@"]
-    with arcpy.da.SearchCursor(Feat, fields, """PLANCHA="""+"'"+plancha+"'") as cursor:
+def QueryPlancha (Feat, plancha, campo):
+    fields=['SHAPE@WKT',campo,"SHAPE@"]
+    with arcpy.da.SearchCursor(Feat, fields, campo+"""="""+"'"+plancha+"'") as cursor:
         for row in cursor:
             print(int(round(row[2].extent.XMin)))
             print(int(round(row[2].extent.YMin)))
@@ -59,19 +67,25 @@ def SeleccionMuestra (capa,numeroMuestra):
     identificador= FindIdentificador(capa)
     fields=[identificador]
     Lista=[]
+    outfc=None
+    arcpy.env.workspace = CarpetaSalida
+    if CarpetaSalida in (".mdb") or CarpetaSalida in (".gdb"):
+        outfc = arcpy.ValidateTableName("Grilla_aleatoria")
+    else:
+        outfc = arcpy.ValidateTableName("Grilla_aleatoria.shp")
     with arcpy.da.SearchCursor(capa, fields) as cursor:
         for row in cursor:
             Lista.append(row[0])
     random.shuffle(Lista)
     ListaAleatoria=Lista[:numeroMuestra]
     #crear Query
-    campo=arcpy.AddFieldDelimiters(salida,identificador)
+    campo=arcpy.AddFieldDelimiters(capa,identificador)
     QueryAleatorio= campo+"in "+str(tuple(ListaAleatoria))
-    arcpy.Select_analysis(capa,CarpetaSalida+os.sep+"Grilla_aleatoria2",QueryAleatorio)
+    arcpy.Select_analysis(capa,outfc,QueryAleatorio)
     
 
 Grilla=CrearSalida(CarpetaSalida,"Grilla2.shp")
-coord=QueryPlancha(Planchas,plancha)
+coord=QueryPlancha(Planchas,plancha, campo)
 CreateFishnet(coord,Grilla,Filas,Columnas)
 
 SeleccionMuestra(Grilla,NumeroMuestra)
