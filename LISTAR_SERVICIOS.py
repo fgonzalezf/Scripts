9dacbbc869c2534f5e4c08bff0923b381489a19b
fgonzalezf <fgonzalezf@sgc.gov.co>
2019-08-12 11:17:58 -0500

    nuevo

--- LISTAR_SERVICIOS.py
+++ LISTAR_SERVICIOS.py
@@ -12,11 +12,11 @@ def main(argv=None):
 
     # Ask for admin/publisher user name and password
     username = r"SitioSGC"
-    password = "sgc2015."
+    password = "sgc2014."
 
     # Ask for server name & port
-    serverName = "172.25.2.142"
-    serverPort = 6443
+    serverName = "srvags.sgc.gov.co"
+    serverPort = 80
 
     # Get the location and the name of the file to be created
     resultFile = r"C:\temp\SERVICIOS.csv"
