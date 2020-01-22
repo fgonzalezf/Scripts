87366866408fb2e665fd21de0e0bd834d623bc94
APN <APN@gmail.com>
2020-01-22 18:39:13 -0500

    renombrado y conversion coordenadas

--- DAICMA/Join_Spatial_Job.py
+++ DAICMA/Join_Spatial_Job.py
@@ -5,6 +5,12 @@ FeatJoin = r"C:\Users\APN\Downloads\GDB\BK_28_10_2019.gdb\DAICMA\Municipios"
 FeatOut=r"in_memory\Join_Feat"
 FeatAppend= r"C:\Users\APN\Downloads\GDB\BK_28_10_2019.gdb\DAICMA\Eventos_Intervencion"
 
+
+
+
+
+
+
 print "Iniciando Proceso"
 arcpy.DeleteFeatures_management(FeatAppend)
 arcpy.SpatialJoin_analysis(FeatIn , FeatJoin ,FeatOut,"JOIN_ONE_TO_ONE","KEEP_ALL","","INTERSECT")
